854894cfe6f28a1b0bf148c3505cfaae3c61a90a
Aaron Hao (hanhao) <hanhao@cisco.com>
2020-07-20 15:02:39 +0800

    03

--- 03.py
+++ 03.py
@@ -3,27 +3,27 @@ def lengthOfLongestSubstring(s):
     :type s: str
     :rtype: int
     """
-    currentString = s[0:1]
-    currentLength = 1
-    maxLength = 1
-    for i, val in enumerate(s):
-        mergedString = mergedString(val, s)
+    currentLength = 0
+    maxLength = 0
+    l = 0
+    r = 0
+    charMap = {}
+    while r < len(s):
+        if s[r] not in charMap or (s[r] in charMap and charMap[s[r]] is False):
+            charMap[s[r]] = True
+            r += 1
+            currentLength += 1
+            maxLength = maxLength if maxLength >= currentLength else currentLength
+        else:
+            while True:
+                charMap[s[l]] = False
+                l += 1
+                currentLength -= 1
+                if s[l] == s[r]:
+                    break
+    return maxLength
 
-
-def s2hex(s):
-    return hex(int(s, 16))
-
-def mergeCharWithString(c, s):
-    index = ord(c) - ord('a')
-    location = index // 4
-    indexHex: str = hex(1 << index % 4)
-    res = hex(int(s, 16) | int(indexHex + '0' * location, 16))
-    if s==res:
-        res = hex(int(s, 16) ^ int(indexHex + '0' * location, 16))
-    return res
-
-def judgeCharBelongs2String(s, c):
-    return True if s == (s & c) else False
 
 class Solution(object):
-    lengthOfLongestSubstring("abc")
+    i = lengthOfLongestSubstring("abcabcbb")
+    print(i)
