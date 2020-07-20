2722ffa84da1d530c7b2eb50ca8fb28549788c1a
Aaron Hao (hanhao) <hanhao@cisco.com>
2020-07-20 15:26:02 +0800

    03

--- 03.py
+++ 03.py
@@ -28,3 +28,23 @@ def lengthOfLongestSubstring(s):
 class Solution(object):
     i = lengthOfLongestSubstring("dvdf")
     print(i)
+
+
+'''
+CaptainTec
+2020-05-02
+哈希Map 只需一次遍历, more efficiency
+附 Python 代码
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        k, res, c_dict = -1, 0, {}
+        for i, c in enumerate(s):
+            if c in c_dict and c_dict[c] > k:  # 字符c在字典中 且 上次出现的下标大于当前长度的起始下标
+                k = c_dict[c]
+                c_dict[c] = i
+            else:
+                c_dict[c] = i
+                res = max(res, i-k)
+        return res
+'''
