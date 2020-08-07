442961b5083da2f1f3170b4a1e807a9b2dc5834b
Aaron Hao (hanhao) <hanhao@cisco.com>
2020-08-07 15:24:42 +0800

    05

--- 05.py
+++ 05.py
@@ -47,3 +47,26 @@ def longestPalindrome(s):
 class Solution(object):
     s = longestPalindrome('ccc')
     print(s)
+
+"""
+DP solution , refer to https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode-solution/
+
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = ""
+        for l in range(n):
+            for i in range(n):
+                j = i + l
+                if j >= len(s):
+                    break
+                if l == 0:
+                    dp[i][j] = True
+                elif l == 1:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+                if dp[i][j] and l + 1 > len(ans):
+                    ans = s[i:j+1]
+        return ans
+"""
