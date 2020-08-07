d47ee4e3381a73323e340ffc8c8aeabe022bef26
Aaron Hao (hanhao) <hanhao@cisco.com>
2020-08-07 15:06:21 +0800

    05

--- 05.py
+++ 05.py
@@ -1,38 +1,49 @@
 def getMax(s, i):
+    j = i
     k = i + 1
-    if len(s) % 2 == 1:
-        curStr = s[i]
-        curLen = 1
-        j = i - 1
-    else:
-        curStr = ''
-        curLen = 0
-        j = i
-    while j >= 0:
+    curLen = 0
+    curStr = ''
+    while j >= 0 and k < len(s):
         if not s[j] == s[k]:
-            return curLen, curStr
+            break
         curStr = s[j] + curStr + s[k]
-        curLen += 1
+        curLen += 2
         j -= 1
         k += 1
-    return curLen, curStr
+
+    _curStr = s[i]
+    _curLen = 1
+    j = i - 1
+    k = i + 1
+    while j >= 0 and k < len(s):
+        if not s[j] == s[k]:
+            break
+        _curStr = s[j] + _curStr + s[k]
+        _curLen += 2
+        j -= 1
+        k += 1
+
+    if curLen > _curLen:
+        return curLen, curStr
+    else:
+        return _curLen, _curStr
 
 
 def longestPalindrome(s):
-    i = len(s) // 2 if len(s) % 2 == 1 else len(s) // 2 - 1
+    i = 0
     maxLength = 0
     maxString = ''
-    while i > 0:
-        if i < maxLength:
+    while i < len(s):
+        if i < maxLength // 2 or len(s) - 1 - i < maxLength // 2:
             return maxString
         curLen, curStr = getMax(s, i)
         maxLength = max(maxLength, curLen)
         if len(curStr) > len(maxString):
             maxString = curStr
-        i -= 1
+        i += 1
     return maxString
 
 
 class Solution(object):
-    s = longestPalindrome('babad')
+    s = longestPalindrome('ccc')
     print(s)
