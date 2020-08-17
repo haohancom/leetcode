98c7ce8eda478ff54e461dbf10740dd696ac4334
Aaron Hao (hanhao) <hanhao@cisco.com>
2020-08-17 21:14:55 +0800

    12 format

--- 12.py
+++ 12.py
@@ -3,6 +3,7 @@ d2 = {1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L', 6: 'LX', 7: 'LXX', 8: 'LXXX', 
 d3 = {1: 'C', 2: 'CC', 3: 'CCC', 4: 'CD', 5: 'D', 6: 'DC', 7: 'DCC', 8: 'DCCC', 9: 'CM', 0: ''}
 d4 = {1: 'M', 2: 'MM', 3: 'MMM'}
 
+
 def intToRoman(num):
     res = ''
     res = d1[num % 10] + res
@@ -22,6 +23,7 @@ def intToRoman(num):
 
     res = d4[num % 10] + res
     return res
+
 
 class Solution(object):
     s = intToRoman(399)
